50b85d88d46fa2190c084ac1c6971673319f2ce5
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-03-04 17:17:50 +0900

    Add unit conversion functionality

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,17 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var input: Double = 0
+    @State private var inputUnit = ""
+    @State private var outputUnit = ""
+    
+    @FocusState private var isFocused: Bool
+    
+    let conversionUnits = ["second", "minute", "hour", "day"]
+    
+    var inputMultiplier: Double {
+        switch inputUnit {
+        case "second":
+            return 1
+        case "minute":
+            return 60
+        case "hour":
+            return 3600
+        case "day":
+            return 86400
+        default:
+            return 1
         }
-        .padding()
+    }
+    
+    var outputMultiplier: Double {
+        switch outputUnit {
+        case "second":
+            return 1
+        case "minute":
+            return 60
+        case "hour":
+            return 3600
+        case "day":
+            return 86400
+        default:
+            return 1
+        }
+    }
+    
+    var outputValue: Double {
+        let baseValue = input * inputMultiplier
+        return baseValue / outputMultiplier
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Value to convert") {
+                    TextField("Input", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                }
+                Section {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(conversionUnits, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(conversionUnits, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                }
+                Section("Output") {
+                    Text(outputValue.formatted())
+                }
+            }
+            .navigationTitle("UnitConverter")
+            .toolbar {
+                if isFocused {
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
     }
 }
-
+    
+    
+    
 #Preview {
     ContentView()
 }
